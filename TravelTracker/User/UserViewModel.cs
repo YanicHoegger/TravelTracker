9ccb2079c840109bd13fbad36c58738a12cce055
Yanic Högger <yanic.hoegger@gmx.ch>
2017-09-30 16:04:09 +0200

    - Made design right - Delete functionality implemented

--- TravelTracker/User/UserViewModel.cs
+++ TravelTracker/User/UserViewModel.cs
@@ -3,11 +3,6 @@ using System.Collections.Generic;
 
 namespace TravelTracker.User
 {
-    public class DisplayAllViewModel
-    {
-        public IEnumerable<UserViewModel> Users { get; set; }
-    }
-
     public class UserViewModel
     {
         public string UserName { get; set; }
