721d22d3ef9960d1147b0b423b39f0797f126d69
Yanic Högger <yanic.hoegger@gmx.ch>
2017-10-01 12:25:50 +0200

    Add TODO's for refactoring

---
+++ TravelTracker/ViewComponents/LoginStatusViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace TravelTracker
+{
+    public class LoginStatusViewComponent : ViewComponent
+	{
+		readonly UserManager<IdentityUser> _userManager;
+		readonly SignInManager<IdentityUser> _signInManager;
+		readonly IUrlHelperFactory _factory;
+
+		public LoginStatusViewComponent(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+            //var helper = _factory.GetUrlHelper(ViewContext);
+            //helper.ActionContext.RouteData.
+            //return null;
+
+            if(_signInManager.IsSignedIn(HttpContext.User))
+            {
+                var user = await _userManager.GetUserAsync(HttpContext.User);
+                return View("LoggedIn", user);
+            }
+            else
+            {
+                return View();
+            }
+		}
+    }
+}
