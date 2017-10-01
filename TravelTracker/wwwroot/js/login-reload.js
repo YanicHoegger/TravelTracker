a46ea1760c2f6c4a0d188b6532aaa14671a4c3de
Yanic Högger <yanic.hoegger@gmx.ch>
2017-10-01 17:33:18 +0200

    login script refactored

--- TravelTracker/wwwroot/js/login-reload.js
+++ TravelTracker/wwwroot/js/login-reload.js
@@ -1,21 +1,15 @@
-﻿$(function(){
-    $("#LoginForm").submit(function(event) {
+﻿$("#LoginForm").submit(function(event) {
 
-        event.preventDefault();
+    event.preventDefault();
 
-        var $form = $(this),
-            emailInput = $form.find("input[name='Email']").val(),
-            passwordInput = $form.find("input[name='Password']").val();
+    var posting =  $.post("login/login", $("#LoginForm").serialize());
+        
+    posting.done(function(data){
 
-        var posting =  $.post("login/login", {email: emailInput, password: passwordInput});
-            
-        posting.done(function(data){
-
-            if(data.startsWith("Redirect")) {
-                window.location.href = data.substring(9, data.length);
-            } else {
-                $("#LoginStatus").html(data);
-            }                    
-        });
-    });     
-});
+        if(data.startsWith("Redirect")) {
+            window.location.href = data.substring(9, data.length);
+        } else {
+            $("#LoginStatus").html(data);
+        }                    
+    });
+}); 
