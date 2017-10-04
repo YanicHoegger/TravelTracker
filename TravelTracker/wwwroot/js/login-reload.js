34dd9b62da71969316fe0763b4ad861351249ebf
Yanic Högger <yanic.hoegger@gmx.ch>
2017-10-04 20:26:13 +0200

    Bugfix login
    Multiple wrong logins now possible

--- TravelTracker/wwwroot/js/login-reload.js
+++ TravelTracker/wwwroot/js/login-reload.js
@@ -1,15 +1,17 @@
-﻿$("#LoginForm").submit(function(event) {
+﻿$("#LoginForm").submit(loginReload);
 
+function loginReload(event){
     event.preventDefault();
-
+    
     var posting =  $.post("login/login", $("#LoginForm").serialize());
-        
+            
     posting.done(function(data){
-
+    
         if(data.startsWith("Redirect")) {
             window.location.href = data.substring(9, data.length);
         } else {
             $("#LoginStatus").html(data);
+            $("#LoginForm").submit(loginReload);
         }                    
     });
-}); 
+}
