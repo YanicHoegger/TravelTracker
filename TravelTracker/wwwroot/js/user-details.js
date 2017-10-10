4ed129a0df48cbe238d22bc373f6defc5c943c55
Yanic Högger <yanic.hoegger@gmx.ch>
2017-10-10 21:19:31 +0200

    Expand accordion when error occurse from post
    When user input is wrong, the user should only be able to edit his wrong input

--- TravelTracker/wwwroot/js/user-details.js
+++ TravelTracker/wwwroot/js/user-details.js
@@ -13,10 +13,24 @@ function removeCollapsedVisibility(e) {
 }
 
 function getElements(e){
-    return $(e.target)
+    var valueElement = $(e.target)
         .prev('.panel-heading')
-        .find('.visibility-toggled');
+        .find('dd.visibility-toggled');
+    var editButtons = $('a.visibility-toggled');
+    return $.merge(valueElement, editButtons);
 }
 
 $('.panel-group').on('hidden.bs.collapse', removeCollapsedVisibility);
 $('.panel-group').on('shown.bs.collapse', addCollapsedVisibility);
+
+$(function() {
+    if($("#NewUserNameInvalid")[0] != null) {
+        $("#collapse1").collapse('show');
+    }
+    if($("#NewEmailInvalid")[0] != null) {
+        $("#collapse2").collapse('show');
+    }
+    if($("#NewPasswordInvalid")[0] != null) {
+        $("#collapse3").collapse('show');
+    }
+});
