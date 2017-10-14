9ef0ef1489028251e4aec32921b8b0d7f0343503
Yanic Högger <yanic.hoegger@gmx.ch>
2017-10-14 21:47:59 +0200

    AccountController UnitTest Refactoring, big Refactoring

--- UnitTests/AccountControllerTests.cs
+++ UnitTests/AccountControllerTests.cs
@@ -1,162 +1,97 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TravelTracker.Controllers;
+using TravelTracker.User;
 using Xunit;
 
 namespace UnitTests
-{    
+{
     public class AccountControllerTests
-    {   
-        
-        [Theory]
-		[InlineData(FieldEnum.EmailAddress, "Email is empty")]
-        [InlineData(FieldEnum.UserName, "User Name is empty")]
-		[InlineData(FieldEnum.Password, "Password is empty")]
-        public void RegisterProduceErrorMessageWhenEmptyFields(FieldEnum fieldEnum, string incorrectMessage)
+    {
+        [Fact]
+        public async Task RegisterSuccessfulTest()
         {
-            GivenCorrectFields();
-            GivenEmptyField(fieldEnum);
+            GivenAccountControllerWhereUserManagerReturnsSuccess();
 
-            WhenRegister();
+            await WhenRegisterAsync();
 
-            ThenResultViewWithIncorrectFieldMessage(incorrectMessage);
+            ThenSuccess();
         }
 
         [Fact]
-        public void RegisterSuccessfulWhenCorrectFields()
-        {
-            GivenCorrectFields();
-            
-            WhenRegister();
+		public async Task RegisterNotSuccessfulWhenModelStateErrorTest()
+		{
+			GivenAccountControllerWithModelStateError();
 
-            ThenResultViewIsSuccessView();
-        }
+			await WhenRegisterAsync();
 
-        //TODO: When have a better solution for genarating unique messages, enable tests again
-        [Theory(Skip = "Is handled by the user Manager")]
-        [InlineData(FieldEnum.EmailAddress, "somethingWithoutAnAt", "")]
-        [InlineData(FieldEnum.Password, "asd", "")]
-        public void RegisterProduceErrorMessageWhenIncorrectFields(FieldEnum fieldEnum, string incorrectValue, string incorrectMessage)
-        {
-            GivenCorrectFields();
-            GivenIncorrectField(fieldEnum, incorrectValue);
-
-            WhenRegister();
-            
-            ThenResultViewWithIncorrectFieldMessage(incorrectMessage);
-        }
+			ThenResultWithModelStateError();
+		}
 
         [Fact]
-        public void RegisterDifferentPasswordAndRepasswordProduceErrorMessage()
+        public async Task RegisterNotSuccessfuWhenUserManagerReturnsErrorTest()
         {
-            GivenDifferentPasswordAndRepassword();
-            
-            WhenRegister();
-            
-            ThenResultViewWithIncorrectFieldMessage("Passwords don't match");
+            GivenAccountControllerWhereUserManagerReturnsErrors();
+
+            await WhenRegisterAsync();
+
+            ThenResultWithModelStateError();
         }
 
-        #region
+		AccountController _accountController;
+		IActionResult _result;
 
-        private readonly AccountControllerMock _accountControllerMock = new AccountControllerMock();
-        private string _emailAddress;
-        private string _userName;
-        private string _password;
-        private string _repassword;
-        private IActionResult _result;
-
-        private void GivenEmptyField(FieldEnum fieldEnum)
+        void GivenAccountControllerWhereUserManagerReturnsSuccess()
         {
-            SetFieldValue(fieldEnum, null);
+			var userManagerMock = new Mock<UserManagerMock>();
+			userManagerMock.Setup(x => x.CreateAsync(It.IsAny<IdentityUser>(), It.IsAny<string>()))
+                           .ReturnsAsync(IdentityResult.Success);
+
+			_accountController = new AccountController(userManagerMock.Object);
         }
 
-        private void GivenCorrectFields()
-        {
-            _emailAddress = "test@test.ch";
-            _userName = "testUserName";
-            _password = "AsDfg";
-            _repassword = "AsDfg";
-        }
+        void GivenAccountControllerWithModelStateError()
+		{
+			_accountController = new AccountController(new Mock<UserManagerMock>().Object);
+			_accountController.ModelState.AddModelError("", "Error");
+		}
 
-        private void GivenIncorrectField(FieldEnum field, string incorrectFieldValue)
+        void GivenAccountControllerWhereUserManagerReturnsErrors()
         {
-            SetFieldValue(field, incorrectFieldValue);
-
-            //Password and repassword have to be the same for this test, 
-            //otherwise an error for not equal password and repassword would occour first
-            if (field.Equals(FieldEnum.Password))
+            var identityError = new IdentityError()
             {
-                SetFieldValue(FieldEnum.Repassword, incorrectFieldValue);
-            }
+                Code = "Code",
+                Description = "Error"
+            };
 
-			//Password and repassword have to be the same for this test, 
-			//otherwise an error for not equal password and repassword would occour first
-            if (field.Equals(FieldEnum.Repassword))
-			{
-                SetFieldValue(FieldEnum.Password, incorrectFieldValue);
-			}
+            var userManagerMock = new Mock<UserManagerMock>();
+			userManagerMock.Setup(x => x.CreateAsync(It.IsAny<IdentityUser>(), It.IsAny<string>()))
+                           .ReturnsAsync(IdentityResult.Failed(identityError));
+
+            _accountController = new AccountController(userManagerMock.Object);
         }
 
-        private void SetFieldValue(FieldEnum field, string fieldValue)
-        {
-			switch (field)
-			{
-				case FieldEnum.EmailAddress:
-					_emailAddress = fieldValue;
-					break;
-                case FieldEnum.UserName:
-                    _userName = fieldValue;
-					break;
-				case FieldEnum.Password:
-					_password = fieldValue;
-					break;
-				case FieldEnum.Repassword:
-					_repassword = fieldValue;
-					break;
-				default:
-					throw new ArgumentOutOfRangeException(nameof(field), field, null);
-			}
-        }
+		async Task WhenRegisterAsync()
+		{
+			_result = await _accountController.Register(new RegisterUserViewModel());
+		}
 
-        private void GivenDifferentPasswordAndRepassword()
-        {
-            _password = "SomethingCorrect";
-            _repassword = "SomethingCorrectButDifferent";
-            _emailAddress = null;
-        }
+		void ThenResultWithModelStateError()
+		{
+			var resultAsViewResult = Assert.IsType<ViewResult>(_result);
+			Assert.False(resultAsViewResult.ViewData.ModelState.IsValid);
 
-        private void WhenRegister()
-        {
-            _result = _accountControllerMock.AccountController.Register(_emailAddress, _userName ,_password, _repassword).Result;
-        }
+			Assert.False(_accountController.ModelState.IsValid);
+		}
 
-        private void ThenResultViewIsSuccessView()
+        void ThenSuccess()
         {
             var resultAsContentResult = Assert.IsType<ContentResult>(_result);
-            
             Assert.Equal("Registering user successful", resultAsContentResult.Content);
         }
-        
-        private void ThenResultViewWithIncorrectFieldMessage(string incorrectMessage)
-        {     
-            Assert.True(GetMessages().Contains(incorrectMessage), string.Format("There is no message '{0}'", incorrectMessage));
-        }
-
-        private IEnumerable<string> GetMessages()
-        {
-            return _accountControllerMock.MessageCollection.Messages.Select(message => message.Message);
-        }
-
-        #endregion
-
-    }
-
-    public enum FieldEnum
-    {
-        EmailAddress,
-        UserName,
-        Password,
-        Repassword
     }
 }
