ef4663883920fca4f2baf6c88e535feaddf28a18
Yanic Högger <yanic.hoegger@gmx.ch>
2017-10-15 20:32:36 +0200

    Add Unittests for Display All Accounts

---
+++ UnitTests/AccountControllerTests/DisplayAllAccountsTests.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TravelTracker.Controllers;
+using TravelTracker.User;
+using Xunit;
+
+namespace UnitTests.AccountControllerTests
+{
+    public class DisplayAllAccountsTests
+    {
+        [Fact]
+        public void ShowAllAccountsFromUserManagerTest()
+        {
+            GivenAccountControllerWithUsers();
+
+            WhenDisplayAll();
+
+            ThenShowAllAccounts();
+        }
+
+        [Fact]
+        public void ShowNoAccountsWhenUserManagerReturnsEmptyList()
+        {
+            GivenAccountControllerWhereUserManagerReturnsNoUsers();
+
+            WhenDisplayAll();
+
+            ThenShowNoAccounts();
+        }
+
+        AccountController _accountController;
+        IActionResult _result;
+
+        IQueryable<IdentityUser> _users = new List<IdentityUser>
+                {
+                    new IdentityUser
+                    {
+                        UserName = "Test1",
+                        Email = "test1@test.it"
+                    },
+                    new IdentityUser
+                    {
+                        UserName = "Test2",
+                        Email = "test2@test.it"
+                    }
+                }.AsQueryable();
+
+        void GivenAccountControllerWithUsers()
+        {
+            var userManagerMock = new Mock<UserManagerMock>();
+            userManagerMock.Setup(x => x.Users).Returns(_users);
+
+            _accountController = new AccountController(userManagerMock.Object)
+            {
+                Url = new UrlHelperMock()
+            };
+        }
+
+        void GivenAccountControllerWhereUserManagerReturnsNoUsers()
+        {
+            var userManagerMock = new Mock<UserManagerMock>();
+		    userManagerMock.Setup(x => x.Users).Returns(new List<IdentityUser>().AsQueryable());
+
+		    _accountController = new AccountController(userManagerMock.Object);
+        }
+
+        void WhenDisplayAll()
+        {
+            _result = _accountController.DisplayAll();
+        }
+
+        void ThenShowAllAccounts()
+        {
+            var viewModelList = GetAndAssertViewModelList();
+
+            AssertIdentityUserAndUserViewModel(_users.First(), viewModelList.First());
+            AssertIdentityUserAndUserViewModel(_users.ElementAt(1), viewModelList.ElementAt(1));
+        }
+
+        void AssertIdentityUserAndUserViewModel(IdentityUser identityUser, UserViewModel viewModel)
+        {
+            Assert.Equal(identityUser.UserName, viewModel.UserName);
+			//TODO: Test for viewModel.RouteUrl
+		}
+
+        void ThenShowNoAccounts()
+        {
+			var viewModelList = GetAndAssertViewModelList();
+
+            Assert.Empty(viewModelList);
+        }
+
+        IEnumerable<UserViewModel> GetAndAssertViewModelList()
+        {
+			var resultAsViewResult = Assert.IsType<ViewResult>(_result);
+			var viewModelList = resultAsViewResult.Model as IEnumerable<UserViewModel>;
+			Assert.NotNull(viewModelList);
+
+            return viewModelList;
+        }
+    }
+}
---
+++ UnitTests/AccountControllerTests/UrlHelperMock.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace UnitTests.AccountControllerTests
+{
+    public class UrlHelperMock : IUrlHelper
+    {
+        public ActionContext ActionContext => throw new NotImplementedException();
+
+        public string Action(UrlActionContext actionContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Content(string contentPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsLocalUrl(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Link(string routeName, object values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RouteUrl(UrlRouteContext routeContext)
+        {
+            if (!routeContext.RouteName.Equals("users"))
+			{
+				throw new ArgumentException("The used router should be 'users'", nameof(routeContext.RouteName));
+			}
+
+			//TODO: Check for routeContext.Values
+
+			return "someString";
+        }
+    }
+}
