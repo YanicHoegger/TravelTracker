55647e1d347afb46ad293156d5db7cf296c05f9c
Yanic Högger <yanic.hoegger@gmx.ch>
2017-10-16 18:45:37 +0200

    ViewModel Tests for UserController

---
+++ UnitTests/UserControllerTests/NewEmailViewModelTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using TravelTracker.User;
+using Xunit;
+
+namespace UnitTests.UserControllerTests
+{
+    public class NewEmailViewModelTests : ViewModelTestBase
+    {
+        [Fact]
+        public void EmptyPropertiesTest()
+        {
+            GivenViewModel(null);
+
+            WhenValidate();
+
+            ThenInvalid("The New Email field is required");
+        }
+
+        [Theory]
+        [InlineData("somethingWithout.at")]
+        [InlineData("something@")]
+        public void WrongEmailTest(string email)
+        {
+            GivenViewModel(email);
+
+			WhenValidate();
+
+            ThenInvalid("This is not a valid e-mail address");
+        }
+
+        [Fact]
+        public void RightEmailTest()
+        {
+            GivenViewModel("somethin@right.com");
+
+            WhenValidate();
+
+            ThenValid();
+        }
+
+        NewEmailViewModel viewModel;
+
+        protected override object ViewModel => viewModel;
+
+        void GivenViewModel(string email)
+        {
+            viewModel = new NewEmailViewModel()
+            {
+                NewEmail = email
+            };
+        }
+    }
+}
