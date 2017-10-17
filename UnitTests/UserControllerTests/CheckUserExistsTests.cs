24777517f91530814987732b51acaa911a59dc20
Yanic Högger <yanic.hoegger@gmx.ch>
2017-10-17 12:45:06 +0200

    Test that makes sure that only actions will be executed with existend user

---
+++ UnitTests/UserControllerTests/CheckUserExistsTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using TravelTracker.Controllers;
+using TravelTracker.User;
+using Xunit;
+
+namespace UnitTests.UserControllerTests
+{
+    public class CheckUserExistsTests
+    {
+        [Fact]
+        public void IndexCheckUserExistTest()
+        {
+            GvienUserController();
+
+            WhenIndex();
+
+            ThenNotFoundResult();
+        }
+
+        [Fact]
+        public async Task ChangeUserNameCheckUserExistTest()
+		{
+			GvienUserController();
+
+            await WhenAction(userController.ChangeUserName);
+
+			ThenNotFoundResult();
+		}
+
+		[Fact]
+		public async Task ChangeEmailCheckUserExistTest()
+		{
+			GvienUserController();
+
+			await WhenAction(userController.ChangeEmail);
+
+			ThenNotFoundResult();
+		}
+
+		[Fact]
+		public async Task ChangePasswordCheckUserExistTest()
+		{
+			GvienUserController();
+
+			await WhenAction(userController.ChangePassword);
+
+			ThenNotFoundResult();
+		}
+
+		//Class could be simplyfied with this method, but in terms of better readable it is not used
+        //TODO: Check lambda and object relation
+        /*async Task UserControllerActionCheckUserExistTest(Func<UserController, Func<IdentityUser, UserDetailsViewModel, Task<IActionResult>>> userControllerActionGetter)
+        {
+			GvienUserController();
+
+            await WhenAction(userControllerActionGetter(userController));
+
+			ThenNotFoundResult();
+        }*/
+
+        UserController userController;
+        IActionResult result;
+
+        void GvienUserController()
+        {
+            userController = new UserController(null);
+        }
+
+        void WhenIndex()
+        {
+            result = userController.Index(null);
+        }
+
+        async Task WhenAction(Func<IdentityUser, UserDetailsViewModel, Task<IActionResult>> userControllerAction)
+		{
+            result = await userControllerAction(null, null);
+		}
+
+        void ThenNotFoundResult()
+        {
+            Assert.IsType<NotFoundResult>(result);
+        }
+    }
+}
